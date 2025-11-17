85cfb8e2b73e3e7fa2170c06722e45d4422a160a
XeyKG <samcam20062004w@gmail.com>
2025-11-17 16:12:42 -0500

    Configuro E2E en Cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "a7bq2k",
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
